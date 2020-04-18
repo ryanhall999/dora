ca2403f74a6875917dadb2a3c155c90d4138c5d4
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-04-18 11:06:35 -0400

    add react bootstrap

---
+++ src/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function LoginScreen() {
+  return (
+    <div>
+      <Button variant="primary">Test</Button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = document.querySelector('#root');
+const root = document.querySelector("#root");
 ReactDOM.render(<App />, root);
