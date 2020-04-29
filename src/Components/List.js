e54dc0176678a83dd49ce6c922c98e8e8a7c95a7
Ryan Hall <ryanhall999@gmail.com>
2020-04-29 17:21:08 -0400

    table fixes

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Axios from "axios";
 import "./List.css";
-import { ListGroupItem, Card, InputGroup, FormControl } from "react-bootstrap";
+import {
+	ListGroupItem,
+	Card,
+	InputGroup,
+	FormControl,
+	Table,
+} from "react-bootstrap";
 
 export default function List() {
 	const [markers, setMarkers] = useState([]);
@@ -20,36 +26,53 @@ export default function List() {
 
 	return (
 		<div>
-			<Card style={{ width: "100%", alignSelf: "center" }}>
+			<Card
+				style={{
+					width: "100%",
+					alignSelf: "center",
+					color: "white",
+					backgroundColor: "#3e444a",
+				}}
+			>
 				<Card.Header
 					style={{ fontWeight: "bold", fontSize: "3rem", alignSelf: "center" }}
 				>
 					Featured Deals!
 				</Card.Header>
-				<ListGroup>
-					{markers.map((marker) => {
-						return (
-							<ListGroupItem className="ListGroupItem" action variant="light">
-								<div style={{ fontWeight: "bold" }}> {marker.name} </div>
-								<div>
-									${marker.price} %{(marker.discount * 100).toFixed(2)} Off
-								</div>
-								<div> {marker.product} </div>
-								<div>
-									<a
-										href={`mailto:?subject=${
-											marker.name
-										} Discount!&body=Check out this deal! ${marker.name} at % ${
-											marker.discount * 100
-										} off!`}
-									>
-										Share this Deal!
-									</a>
-								</div>
-							</ListGroupItem>
-						);
-					})}
-				</ListGroup>
+				<Table striped bordered hover variant="dark">
+					<thead>
+						<tr>
+							<th>Item</th>
+							<th>Deal</th>
+							<th>Type</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{markers.map((marker) => {
+							return (
+								<tr>
+									<td style={{ fontWeight: "bold" }}> {marker.name} </td>
+									<td>
+										${marker.price} %{(marker.discount * 100).toFixed(2)} Off
+									</td>
+									<td> {marker.product} </td>
+									<td>
+										<a
+											href={`mailto:?subject=${
+												marker.name
+											} Discount!&body=Check out this deal! ${
+												marker.name
+											} at % ${marker.discount * 100} off!`}
+										>
+											Share this Deal!
+										</a>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
 			</Card>
 		</div>
 	);
