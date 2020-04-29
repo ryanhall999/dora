41235221aa9fdd4ae710b9452b586702e5295f08
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-04-29 10:56:09 -0400

    testing heroku loggin

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,29 +1,29 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('../keys')
-const db = require('../db')
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../keys");
+const db = require("../db");
 
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  db.findUser(user.googleid)
-    .then(user => {
-      done(null, user)
-    })
+  db.findUser(user.googleid).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    callbackURL: "http://localhost:3000/google/callback"
-
-  }, (accessToken, refreshToken, profile, done) => {
-    db.createUser(profile.id, profile.displayName)
-    .then(user => {
-      done(null, user)
-    })
-  })
-)
+  new GoogleStrategy(
+    {
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+      callbackURL: "https://headcount-discount.herokuapp.com/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      db.createUser(profile.id, profile.displayName).then(user => {
+        done(null, user);
+      });
+    }
+  )
+);
